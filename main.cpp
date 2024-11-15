fe70b995d1ebf5acce9bd210fbc4feb6608d3670
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-11-15 10:06:58 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,22 @@
 using namespace std;
 #define rp(i,a,b)   for(int i=a; i<b; i++)
 typedef vector<int> vi;
-
 const int SIZE = 4;
 
+//lab pr1
 double sumColumn(double m[][SIZE], int rowSize, int columnIndex) {
     double sum = 0.0;
     if (columnIndex < 0 || columnIndex >= SIZE){    cout << "Invalid column index!" << endl;    return 0.0;}
     rp(i,0, rowSize){   sum += m[i][columnIndex];}
     return sum;
 }
+//lab pr2
+double sumMajorDiagonal(const double m[][SIZE]) {
+    double sum = 0.0;
+    for (int i = 0; i < SIZE; i++){    sum += m[i][i];}
+    return sum;
+}
+
 
 void pr1() {
     int m, n;  cin >> m >> n;
